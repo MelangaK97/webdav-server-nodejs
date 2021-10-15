0aaaeb5b7f4625f471719eb5a944a8d8d7b6ef4c
Udara <hwupathum@gmail.com>
2021-10-15 18:58:40 +0530

    removed webdav server

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,37 @@
 // TypeScript
-import { v2 as webdav } from 'webdav-server';
 import express from 'express';
 import mountSeadrive from './seafile_utils/mountSeadrive';
 import createConfig from './seafile_utils/createConfig';
+import getToken from './seafile_utils/getToken';
+import * as crypto from 'crypto'
 
-// User manager (tells who are the users)
-const userManager = new webdav.SimpleUserManager();
- 
-// Privilege manager (tells which users can access which files/folders)
-const privilegeManager = new webdav.SimplePathPrivilegeManager();
+const host = 'http://localhost:8080';
+const realm = 'Seadrive';
+
+// database
+const users =  new Set<{username: string, password: string}>();
  
 const app = express();
 
-const server = new webdav.WebDAVServer({
-    // HTTP Digest authentication with the realm 'Default realm'
-    httpAuthentication: new webdav.HTTPDigestAuthentication(userManager, 'Default realm'),
-    privilegeManager: privilegeManager,
-    // port: 1900, // Load the server on the port 2000 (if not specified, default is 1900)
-    autoSave: { // Will automatically save the changes in the 'data.json' file
-        treeFilePath: 'data.json'
-    }
-});
 
-const user = userManager.addUser('username', 'password', false);
-privilegeManager.setRights(user, '/', [ 'all' ]);
+
 
 // createConfig('/home/udara/sample.conf', 'http://localhost:8080', 'me@example.com', '1234')
 
-mountSeadrive('~/seadrive.conf', '~/Documents/FYP/Seadrive', '~/Seadrive', '~/Documents/FYP/log.txt', true)
+// mountSeadrive('~/seadrive.conf', '~/Documents/FYP/Seadrive', '~/Seadrive2', '~/Documents/FYP/log.txt', true)
 
-// // server.setFileSystemSync('/Downloads', new webdav.PhysicalFileSystem('/home/udara/Downloads'))
-server.setFileSystemSync('/', new webdav.PhysicalFileSystem('/home/udara/Seadrive'))
-// // server.setFileSystem('/', new webdav.PhysicalFileSystem('/home/udara/Downloads'), (success) => {
-// //     server.start(() => console.log('READY'));
-// // });
+// server.setFileSystemSync('/drive', new webdav.PhysicalFileSystem('/home/udara/Seadrive'))
 
-// app.get('/login', function (req, res) {
-//     res.send('hello world')
+// app.post('/login', function (req, res) {
+//     let username = req.body.username;
+//     let password = req.body.password;
+//     // check if user already registered
+//     const token  = getToken(username, password);
+//     createConfig('/home/udara/sample.conf', host, username, token, username);
+//     // add to database
+//     const passwordHash = bcrypt.hashSync(password, 10);
+//     users.add({username, password: passwordHash});
+//     res.sendStatus(400);
 // })
-app.use(webdav.extensions.express('/', server))
 
 app.listen(1901)
