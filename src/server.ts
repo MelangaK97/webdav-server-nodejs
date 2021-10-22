8888583704f3c65a18a988fa6d9853a250722d06
Melanga Kasun <melangakasun@gmail.com>
2021-10-22 17:19:26 +0530

    moved the user saving part

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ app.post('/login', async function (req, res) {
     let username: string = req.body.username;
     let password: string = req.body.password;
     console.log(`Login request... Username: ${username}, Password: ${password}`);
+    let response: any;
     
     // check if user already registered
     getToken(host, username, password, async (error: any, stdout: any, stderr: any) => {
@@ -38,6 +39,7 @@ app.post('/login', async function (req, res) {
             let opt: any = JSON.parse(stdout);
             if (opt.non_field_errors) {
                 console.error(`An error occurred... Username: ${username}, Error: ${opt.non_field_errors}`);
+                response = opt.non_field_errors;
                 res.statusCode = 401;
             } else if (opt.token) {
                 console.log(`Successfully logged in... Username: ${username}, Token: ${opt.token}`);
@@ -60,29 +62,31 @@ app.post('/login', async function (req, res) {
                         let salt: string = crypto.randomBytes(16).toString('hex'); 
                         // Hashing user's salt and password with 1000 iterations, 
                         let hash: string = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
-
-                        // Saving user data in Redis
-                        console.log(`Adding User: ${username} to the database...`);
-                        let user: User = new User(username, password, userDir, true);
-                        let newUser = {"Username": username, "Password": password, "Scope": userDir, "Modify": "true"};
-                        users.set(username, user);
-                        // await jsonCache.set('users', users);
-                        redisClient.hset('users', username, JSON.stringify(newUser), (error, reply) => {
-                            if (error) {
-                                console.error(error);                                    
-                            }
-                        });
-                        console.log(`Successfully logged in...`);
-                        res.statusCode = 200;
                     }, 
                     `${baseDir}/${username}/seadrive.log`, true);
+
+                    // Saving user data in Redis
+                    console.log(`Adding User: ${username} to the database...`);
+                    let user: User = new User(username, password, userDir, true);
+                    let newUser = {"Username": username, "Password": password, "Scope": userDir};
+                    users.set(username, user);
+                    // await jsonCache.set('users', users);
+                    redisClient.hset('users', username, JSON.stringify(newUser), (error, reply) => {
+                        if (error) {
+                            console.error(error);                                    
+                        }
+                    });
+                    console.log(`Successfully logged in...`);
+                    res.statusCode = 200;
+                    
                 } catch(error) {
                     console.error(`An error occurred... ${error}`);
+                    response = error;
                     res.statusCode = 500;
                 }
             }
         }
-        res.send();
+        res.send(response);
     });
 });
 
