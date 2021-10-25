a04ee260d4f4802445c7350c79147961dddd8997
Melanga Kasun <melangakasun@gmail.com>
2021-10-25 17:37:54 +0530

    Mounted seadrive locations for saved uses when rerver restarts.

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,20 @@ import getToken from './seafile_utils/getToken';
 import fs from 'fs';
 import redis from 'redis';
 import * as crypto from 'crypto';
-import {User} from './user';
+import { exec } from 'child_process';
 
 const host = 'http://localhost:7080';
-const baseDir = '/home/melangakasun/Desktop/FYP/test';
+const data_directory = '/home/melangakasun/.seadrive/data';
+const base_directory = '/home/melangakasun/Desktop/FYP/test';
 const realm = 'Seadrive';
+const server_port = 1901;
 const redis_port = 6379;
-
-// database
-const users =  new Map<string, User>();
  
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-const redisClient = redis.createClient(redis_port);
+const redis_client = redis.createClient(redis_port);
 
 app.post('/login', async function (req, res) {
     let username: string = req.body.username;
@@ -43,39 +42,30 @@ app.post('/login', async function (req, res) {
                 res.statusCode = 401;
             } else if (opt.token) {
                 console.log(`Successfully logged in... Username: ${username}, Token: ${opt.token}`);
-                let userDir: string = `${baseDir}/${username}/data`;
+                let user_directory: string = `${base_directory}/${username}/data`;
                 try {
-                    if (!fs.existsSync(userDir)) {
+                    if (!fs.existsSync(user_directory)) {
                         // creating a directory for the user
-                        console.log(`Creating directory... ${userDir}`);
-                        fs.mkdirSync(userDir, { recursive: true });
+                        console.log(`Creating directory... ${user_directory}`);
+                        fs.mkdirSync(user_directory, { recursive: true });
                     }
-                    if (!fs.existsSync(`${baseDir}/${username}/seadrive.conf`)) {
+                    if (!fs.existsSync(`${base_directory}/${username}/seadrive.conf`)) {
                         // creating the seadrive.conf file
                         console.log(`Creating the configuration file...`);
-                        createConfig(`${baseDir}/${username}/seadrive.conf`, host, username, opt.token, username);
+                        createConfig(`${base_directory}/${username}/seadrive.conf`, host, username, opt.token, username);
                     }
                     // Mounting the directory
                     console.log(`Mounting with the Seadrive directory...`);
-                    mountSeadrive(`${baseDir}/${username}/seadrive.conf`, '/home/melangakasun/.seadrive/data', userDir, () => {
+                    mountSeadrive(`${base_directory}/${username}/seadrive.conf`, data_directory, user_directory, () => {
                         // Creating a unique salt for a particular user 
                         let salt: string = crypto.randomBytes(16).toString('hex'); 
                         // Hashing user's salt and password with 1000 iterations, 
                         let hash: string = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
                     }, 
-                    `${baseDir}/${username}/seadrive.log`, true);
+                    `${base_directory}/${username}/seadrive.log`, true);
 
-                    // Saving user data in Redis
-                    console.log(`Adding User: ${username} to the database...`);
-                    let user: User = new User(username, password, userDir, true);
-                    let newUser = {"Username": username, "Password": password, "Scope": userDir};
-                    users.set(username, user);
                     // await jsonCache.set('users', users);
-                    redisClient.hset('users', username, JSON.stringify(newUser), (error, reply) => {
-                        if (error) {
-                            console.error(error);                                    
-                        }
-                    });
+                    saveUserInRedis(username, password, user_directory);
                     console.log(`Successfully logged in...`);
                     res.statusCode = 200;
                     
@@ -90,4 +80,46 @@ app.post('/login', async function (req, res) {
     });
 });
 
-app.listen(1901);
+app.listen(server_port, () => mountDirectoriesForSavedUsers());
+
+function mountDirectoriesForSavedUsers(): void {
+    redis_client.hgetall('users', async (error, data) => {
+        if (error) {
+            console.error(error);            
+        } else if (data) {
+            console.log(data);  
+            for (let [key, value] of Object.entries(data)) {
+                try {
+                    let user_data = JSON.parse(value);
+                    await unmountDirectory(user_data.Scope);
+                    mountSeadrive(`${base_directory}/${key}/seadrive.conf`, data_directory, user_data.Scope, () => {
+                        console.log(`Mounted directory for ${key} successfully...`);                        
+                    },
+                    `${base_directory}/${key}/seadrive.log`, true);
+                } catch (error) {
+                    console.error(error);                    
+                }
+            }          
+        }
+    });
+}
+
+function saveUserInRedis(username: string, password: string, directory: string): void {
+    console.log(`Adding User: ${username} to the database...`);
+    let new_user = {"Username": username, "Password": password, "Scope": directory};
+    redis_client.hset('users', username, JSON.stringify(new_user), (error, reply) => {
+        if (error) {
+            console.error(error);                                    
+        }
+    });
+}
+
+async function unmountDirectory(directory: string) {
+    try {
+        let command: string = `fusermount -uz ${directory}`
+        exec(command);   
+        console.log(`${directory} is unmounted successfully...`);
+    } catch (error) {
+        console.error(error);  
+    }   
+}
