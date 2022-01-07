27e30f04a9fc9c7a742e381f06be92ce65c156d7
Melanga Kasun <melangakasun@gmail.com>
2022-01-07 20:29:54 +0530

    updated seadrive mount with ssh support

--- src/seafile_utils/mountSeadrive.ts
+++ src/seafile_utils/mountSeadrive.ts
@@ -1,6 +1,15 @@
-import { exec } from "child_process";
+require('dotenv').config();
 
-export default async function mountSeadrive(config: string, dataDir: string, virtualDriveDir: string, logFile?: string, isForeground?: boolean, callback?: Function) {
-    let command: string = `seadrive -c ${config} ${isForeground ? '-f' : ''} -d ${dataDir} ${logFile ? `-l ${logFile}` : ''} ${virtualDriveDir}`;
-    exec(command, (error, stdout, stderr) => callback && callback(error, stdout, stderr));
+const SSH = require('simple-ssh');
+const ssh = new SSH({ host: `${process.env.HOST_IP_ADDRESS}`, user: `${process.env.HOST_USERNAME}`, pass: `${process.env.HOST_PASSWORD}` });
+const data_directory = process.env.SEAFILE_DATA_DIRECTORY ?? '~/.seadrive/data';
+
+export default function mountSeadrive(config: string, virtualDriveDir: string, logFile?: string, isForeground?: boolean) {
+    console.log(`Mounting seadrive to ${virtualDriveDir}...`);
+    let command: string = `seadrive -c ${config} ${isForeground ? '-f' : ''} -d ${data_directory} ${logFile ? `-l ${logFile}` : ''} ${virtualDriveDir}`;
+    ssh.exec(command, {
+        out: function (stdout: any) { console.log(stdout); },
+        err: function (stderr: any) { console.log(`An error occurred ${stderr}`); },
+        exit: function (code: any) { console.log(code === 0 ? `Mounted directory ${virtualDriveDir} successfully...` : `code ${code}`); },
+    }).start();
 }
